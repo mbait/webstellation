063861fd230a5987f89fb10214c512c9ac327a63
ASolovets <ASolovets@7d8e687a-a1bc-11de-bfb1-cf57f9aef694>
2009-09-21 07:56:12 +0000

    New interface

--- trunk/ws_server.pl
+++ trunk/ws_server.pl
@@ -3,12 +3,72 @@
 use strict;
 use JSON::XS;
 
-use Request;
-
 sub	response {
 	my $var = decode_json $_[0];
-	my $act = $var->{'request'};
-    $act Request $var;	
+	my $act = 'Request::'.$var->{'action'};
+	{
+		no strict 'refs';
+		&$act;
+	}
 }
 
-response encode_json { request => 'register' };
+use Test::More qw 'no_plan';
+
+sub test {
+	 my $r = decode_json (response (encode_json ({ userName => 'Jayson', action => 'register' })));
+	 is $r->{'result'}, 'OK';
+
+	  $r =  (response (encode_json ({ action => 'getUsers'})));
+	  print $r;
+}
+
+test;
+
+#response encode_json { request => 'register' };
+
+package Request;
+
+use strict;
+use Tie::DB_Lock;
+use DB_File;
+use Data::Dumper;
+use JSON::XS;
+
+our $filename = 'data';
+
+sub register {
+	my %hash;
+	tie(%hash, 'Tie::DB_Lock', 'data', 'rw') or die 'Failed to open database';
+	my @players;
+	eval $hash{'players'} if exists $hash{'players'};
+	my $args = decode_json($_[0]);
+
+	# search duplicates
+	my %users;
+	$users{$_} = 1 for @players;
+	if(exists $users{$args->{'userName'}}) {
+		untie %hash;
+		return encode_json { result => 'alreadyTaken' };
+	}
+
+	push @players, $args->{'userName'};
+	$hash{'players'} = Data::Dumper->Dump([\@players], ['*players']);
+	untie %hash;
+	return encode_json {
+		result => 'OK'
+	}
+}
+
+sub getUsers {
+	my %hash;
+	tie(%hash, 'Tie::DB_Lock', 'data', 'r') or die 'Filed to open database';
+	my @players;
+	eval $hash{'players'};
+	untie %hash;
+	return encode_json { 
+		users => \@players,
+		result => 'OK'
+	};
+}
+
+1;

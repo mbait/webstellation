b411bec0613de668bc0d29b253ef4de431edc521
ASolovets <ASolovets@7d8e687a-a1bc-11de-bfb1-cf57f9aef694>
2009-10-10 23:58:19 +0000

    final

--- branches/ws-23.09.09/server.pl
+++ branches/ws-23.09.09/server.pl
@@ -9,11 +9,12 @@ use HTTP::Headers;
 use HTTP::Response;
 use Webstellation::Request;
 use Data::Dumper;
+use URI::Escape;
 
 my %conf;
 if(open FH, 'server.conf') {
 	undef $/;
-	for(grep {!/^#/} split /\n/, <FH>) {
+	for(grep {!/\s*^#/} split /\n/, <FH>) {
 		/(.*?)=(.*)/;
 		$conf{$1} = $2;
 	}
@@ -21,36 +22,36 @@ if(open FH, 'server.conf') {
 }
 else {
 	$conf{'port'} = '4440';
-	$conf{'dbfile'} = 'data.db';
+	$conf{'db'} = 'data.db';
 	warn "Failed to read server.conf: $!\nUsing defaults\n";
 }
 
-our $pid = fork; 
-die "Failed to fork: $!\n" unless defined $pid;
-if($pid) {
-	# Kill child on ^C
-	$SIG{INT} = sub { kill $pid; };
-	# We are parent and must wait
-	waitpid $pid, 0;
-	print "\nShuting down...\n";
-}
-else {
-	print "Starting game server...\n";
-	my $g = Webstellation::Request->new($conf{'dbfile'});
-	print "Starting web server...\n";
-	my $d = HTTP::Daemon->new(LocalPort => $conf{'port'}) or die $!;
-	print "Listening on port $conf{'port'}\n";
+my $g = Webstellation::Request->new($conf{'db'});
+print "Starting server...\n";
+my $d = HTTP::Daemon->new(LocalPort => $conf{'port'}, Reuse => 1) or die $!;
+print "Listening on port $conf{'port'}\n";
 
-	while(my $c = $d->accept) {
-		my $r = $c->get_request;
-		$c->send_error(RC_NOT_IMPLEMENTED) unless $r->method eq 'POST';
-		$c->send_error(RC_FORBIDDEN) unless $r->uri->path eq '/';
-
-		$r->content =~ /r=(.*)/;
-		print "$1\n";
-		$c->send_response(HTTP::Response->new(200, status_message(200),
-			   HTTP::Headers->new(Content_Type => 'text/plain'), $g->dispatch($1)));
+while(my $c = $d->accept) {
+	my $pid;
+	if($pid = fork) {
 		$c->close;
-		undef $c;
+		next;
 	}
+	defined($pid) or die "Failed to fork: $!\n";
+
+	my $r = $c->get_request;
+	$c->send_error(RC_NOT_IMPLEMENTED) unless $r->method eq 'POST';
+	$c->send_error(RC_FORBIDDEN) unless $r->uri->path eq '/';
+
+	$r->content =~ /r=(.*)/;
+	unless(defined $1) {
+		warn "Request variable is missing\n";
+		next;
+	}
+	my $json = uri_unescape $1;
+	print "$json\n";
+	$c->send_response(HTTP::Response->new(200, status_message(200),
+		   HTTP::Headers->new(Content_Type => 'text/plain'), $g->dispatch($json)));
+	$c->close;
+	exit;
 }

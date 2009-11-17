38a783679b3a0948d7d0d39ad2f10f1f4bbd5dcf
ASolovets <ASolovets@7d8e687a-a1bc-11de-bfb1-cf57f9aef694>
2009-11-17 12:40:50 +0000

    Basic oop-structure

---
+++ branches/ws-oop/Webstellation/Request/ClearAll.pm
@@ -0,0 +1,14 @@
+package Webstellation::Request::ClearAll;
+
+use strict;
+use warnings;
+
+sub new {
+	my $inv = shift;
+	my $class = ref $inv || $inv;
+	my $self;
+	bless $self, $class;
+	$self->{db} + 
+}
+
+1;

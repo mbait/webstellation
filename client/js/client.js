826184c579152195e0477519f8a851a2dafc688d
ASolovets <ASolovets@7d8e687a-a1bc-11de-bfb1-cf57f9aef694>
2009-11-25 14:46:24 +0000

    nearly catched up old result

--- client/js/client.js
+++ client/js/client.js
@@ -1,9 +1,10 @@
 var WSClient = new Class.create({
 	initialize: function(obj) {
+		this.updateTime = 1000;
 		this.ui_handler = obj;
 	},
 
-	sendData: function(data, callback) {
+	sendRequest: function(data, callback) {
 		new Ajax.Request(this.host, {
 			method: 'post',
 			parameters: {r: $H(data).toJSON()},
@@ -15,23 +16,43 @@ var WSClient = new Class.create({
 		});
 	},
 	
-	connect: function(host, user) {
-		this.host = host;
-		this.user = user;
-		this.sendData({action: 'register', userName: this.user}, function(data) {this.onRegister(data);});
+	connect: function(data) {
+		this.host = data.host;
+		this.user = data.user;
+		var obj = this;
+		this.sendRequest({action: 'register', userName: this.user}, function(data) {obj.onRegister(data)});
 	},
 
 	disconnect: function() {
-		this.sendData({action: 'logout', userName: this.user});
+		this.sendRequest({action: 'logout', userName: this.user});
 		this.ui_handler.onLogout();
 	},
 
+	createGame: function(data) {
+					data.action = 'createGame';
+					data.userName = this.user;
+					this.sendRequest(data);
+				},
+
 	onRegister: function(data) {
-					alert(data);
 		if(!(data.result == 'ok' || data.result == 'alreadyTaken')) {
 			return;
 		}
-		alert(this);
+		this.startUpdate();
 		this.ui_handler.onRegister();
-	}
+	},
+
+	startUpdate: function() {
+					 this.doUpdate = true;
+					 this.update('Users');
+					 this.update('Maps');
+					 this.update('Games');
+				 },
+
+	update: function(list) {
+					 if(!this.doUpdate) { return }
+					 var obj = this;
+					 this.sendRequest({action: 'get' + list}, function(data) {obj.ui_handler.render(data)});
+					 setTimeout(function() {obj.update(list)}, this.updateTime);
+				 }
 });
--- client/js/ui.js
+++ client/js/ui.js
@@ -8,5 +8,41 @@ var WSUI = new Class.create({
 
 		onRegister: function() {
 			$('auth').hide();
-		}
+		},
+
+		onLogout: function() {
+			$('auth').show();
+		},
+
+		render: function(data) {
+			if(data.users != null) {
+				var obj = $('userlist');
+				obj.update('');
+				$A(data.users).each(function(val) {obj.insert(new Element('a', {class: 'user', href: '#'}).update(val))});
+			}
+			else if(data.maps != null) {
+				var obj = $('mapname');
+				var old = obj.value;
+				obj.update('');
+				$A(data.maps).each(function(val) {obj.insert(new Element('option', {value: val}).update(val))});
+				obj.value = old;
+			}
+			else if(data.games != null) {
+				var obj = $('games');
+				obj.update('');
+				$A(data.games).each(function(val) {obj.insert(new Element('tr').update(new Element('td').update(val)))});
+			}
+		},
+
+		loginInfo: function() {
+					   return { host: $('host').value, user: $('user').value }; 
+				   },
+
+		newGameInfo: function() {
+						 return { 
+							gameName: $('gamename').value,
+							mapName: $('mapname').value,
+							maxPlayers: $('maxplayers').value,
+						 }
+					 }
 	});

0044e490e2c25b42205c6b44f3723845d1046fa0
ASolovets <ASolovets@7d8e687a-a1bc-11de-bfb1-cf57f9aef694>
2009-11-28 10:53:52 +0000

    Fetching game states...

--- client/js/client.js
+++ client/js/client.js
@@ -3,7 +3,7 @@ var WSClient = new Class.create({
 		this.updateTime = 1000;
 		this.ui_handler = obj;
 	},
-
+	// helper methods
 	sendRequest: function(data, callback) {
 		new Ajax.Request(this.host, {
 			method: 'post',
@@ -15,7 +15,7 @@ var WSClient = new Class.create({
 			}
 		});
 	},
-	
+	// main actions
 	connect: function(data) {
 		this.host = data.host;
 		this.user = data.user;
@@ -33,15 +33,28 @@ var WSClient = new Class.create({
 					data.userName = this.user;
 					this.sendRequest(data);
 				},
-
+	// callbacks
 	onRegister: function(data) {
-		if(!(data.result == 'ok' || data.result == 'alreadyTaken')) {
-			return;
-		}
-		this.startUpdate();
-		this.ui_handler.onRegister();
-	},
+					if(!(data.result == 'ok' || data.result == 'alreadyTaken')) {
+						return;
+					}
+					this.startUpdate();
+					this.ui_handler.onRegister();
+				},
 
+	onUpdateReceive: function(data) {
+						 this.ui_handler.render(data);
+						 if(data.games != null) {
+							 var obj = this;
+							 $A(data.games).each(function(val) {
+									 obj.sendRequest({action: 'getGameInfo', gameName: val},
+										 function(data) {
+										 	obj.ui_handler.render(data);
+										 })
+									 });
+						 }
+					 },
+	// updates
 	startUpdate: function() {
 					 this.doUpdate = true;
 					 this.update('Users');
@@ -52,7 +65,7 @@ var WSClient = new Class.create({
 	update: function(list) {
 					 if(!this.doUpdate) { return }
 					 var obj = this;
-					 this.sendRequest({action: 'get' + list}, function(data) {obj.ui_handler.render(data)});
+					 this.sendRequest({action: 'get' + list}, function(data) {obj.onUpdateReceive(data)});
 					 setTimeout(function() {obj.update(list)}, this.updateTime);
 				 }
 });

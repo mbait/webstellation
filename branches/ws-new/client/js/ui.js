51726d6979931e0676a3ab75740771c622bed1a0
ASolovets <ASolovets@7d8e687a-a1bc-11de-bfb1-cf57f9aef694>
2009-12-04 13:34:57 +0000

    Map as standalone class

--- branches/ws-new/client/js/ui.js
+++ branches/ws-new/client/js/ui.js
@@ -1,13 +1,115 @@
+var Map = new Class.create({
+	initialize: function() {
+		this.canvasWidth = 400;
+		this.canvasHeight = 300;
+		this.planetRadius = 7;
+		this.planetMaxRadius = this.planetRadius*3;
+		this.planetMaxRadius2 = this.planetMaxRadius*2;
+		this.baseRadius = 3;
+		this.baseRadius2 = this.baseRadius * 2;
+		this.clientWidth = this.canvasWidth - this.planetMaxRadius2;
+		this.clientHeight = this.canvasHeight - this.planetMaxRadius2;
+		this.canvas = new Raphael("map", this.canvasWidth, this.canvasHeight);
+		this.bases = new Array();
+		this.planets = new Array();
+	},
+
+	init: function(map, callback) {
+		this.map = map;
+		this.onPlanetClick = callback;
+	},
+
+	addPlanet: function(x, y, r, index) {
+		 var p =  this.canvas.circle(x, y, r*this.planetRadius);
+		 p.attr('fill', '#A9A9A9');
+		 var self = this;
+		 p.node.onclick = function() { self.onPlanetClick(index) }
+		 p.node.onmouseover = function() { p.attr('stroke-width', '2') };
+		 p.node.onmouseout = function() { p.attr('stroke-width', '1') };
+		 this.planets.push({'x': x, 'y': y});
+	},
+
+	addBase: function(x, y) {
+		var base = this.canvas.circle(x, y, this.baseRadius);
+		base.attr('fill', 'red');
+		this.bases.push(base.node);
+	},
+
+	draw: function() {
+		var canvas = this.canvas;
+		var maxx = -32768;
+		var maxy = maxx;
+		var minx = 32768;
+		var miny = minx;
+		$A(this.map.planets).each(function(planet) {
+			maxx = maxx < planet.x ? planet.x : maxx;
+			maxy = maxy < planet.y ? planet.y : maxy;
+			minx = minx > planet.x ? planet.x : minx;
+			miny = miny > planet.y ? planet.y : miny;
+		});
+		var planets = this.map.planets;
+		var w = maxx - minx;
+		var h = maxy - miny;
+		var mx = this.clientWidth / w;
+		var my = this.clientHeight / h;
+		var dx = this.planetMaxRadius;
+		var dy = this.planetMaxRadius;
+		var self = this;
+		var spares = {};
+		var planet_ind = 0;
+		$A(this.map.planets).each(function(planet) {
+			spares[planet_ind] = {};
+			$A(planet.neighbors).each(function(nb) {
+				if(spares[nb] == null || spares[nb][planet_ind] == null) {
+					var t = new Template('M#{x0} #{y0}L#{x1} #{y1}');
+					try {
+					canvas.path(t.evaluate({
+					x0: planet.x*mx + dx,
+					y0: planet.y*my + dy,
+					x1: planets[nb].x*mx + dx,
+					y1: planets[nb].y*my + dy
+					}));
+					spares[planet_ind][nb] = 1; } catch(err) { alert(err) }
+				}
+			 });
+		});
+		var index = 0;
+		$A(this.map.planets).each(function(planet) {
+			 //var c = canvas.circle(planet.x*mx + dx , planet.y*my + dy , planet.size*self.planetRadius);
+			 self.addPlanet(planet.x*mx + dx, planet.y*my + dy, planet.size, index++);
+		});
+	},
+
+	update: function(planets) {
+		var tag = $('map');
+		$A(this.bases).each(function(node) {
+			node.parentNode.removeChild(node);
+		});
+		this.bases.length = 0;
+		var self = this;
+		var index = 0;
+		$A(planets).each(function(state) {
+			var planet = self.planets[index];
+			if(state.bases == 1) {
+				self.addBase(planet.x, planet.y);
+			}
+			else if(state.bases == 2) {
+				self.addBase(planet.x - self.baseRadius*2, planet.y);
+				self.addBase(planet.x + self.baseRadius*2, planet.y);
+			}
+			else if(state.bases == 3) {
+				self.addBase(planet.x, planet.y - self.baseRadius*2);
+				self.addBase(planet.x - self.baseRadius*2, planet.y + self.baseRadius*2);
+				self.addBase(planet.x + self.baseRadius*2, planet.y + self.baseRadius*2);
+			}
+			++index;
+		});
+	}
+});
+
 var WSUI = new Class.create({
 		lookAndFeel: function() {
-			this.canvasWidth = 800;
-			this.canvasHeight = 600;
-			this.planetRadius = 7;
-			this.planetMaxRadius = this.planetRadius*3;
-			this.planetMaxRadius2 = this.planetMaxRadius*2;
-			this.clientWidth = this.canvasWidth - this.planetMaxRadius2;
-			this.clientHeight = this.canvasHeight - this.planetMaxRadius2;
-			this.gameCanvas = new Raphael("map", this.canvasWidth, this.canvasHeight);
+			this.map = new Map();
 		},
 
 		toggle: function(id) {
@@ -66,6 +168,30 @@ var WSUI = new Class.create({
 						}
 				});
 			}
+			else if(data.game != null) {
+				var game = data.game;
+				try {
+				this.map.update(game.planets); } catch(err) { alert(err) }
+				var score = $('score');
+				score.innerHTML = '';
+				for(var i=0; i<this.players.size(); ++i) {
+					var row = new Element('tr');
+					row.update(new Element('td', {colspan: '2', class: 'score'})).update(this.players[i].name);
+					score.insert(row);
+					row = new Element('tr');
+					row.insert(new Element('td').update('Planets:'));
+					row.insert(new Element('td').update(game.score[i].planets));
+					score.insert(row);
+					row = new Element('tr');
+					row.insert(new Element('td').update('Bases:'));
+					row.insert(new Element('td').update(game.score[i].bases));
+					score.insert(row);
+					row = new Element('tr');
+					row.insert(new Element('td').update('Influence:'));
+					row.insert(new Element('td').update(game.score[i].influence));
+					score.insert(row);
+				}
+			}
 		},
 
 		showGameUsers: function(game) {
@@ -85,57 +211,11 @@ var WSUI = new Class.create({
 						   this.activeGame = game.name;
 					   },
 
-		play: function(data) {
-				  this.map = data.map;
-				  this.drawMap();
+		play: function(players, map, callback) {
+				  this.players = players;
+				  this.map.init(map, callback);
+				  this.map.draw();
 			  },
-
-		drawMap: function() {
-					 var canvas = this.gameCanvas;
-					 var maxx = -32768;
-					 var maxy = maxx;
-					 var minx = 32768;
-					 var miny = minx;
-					 var m = function(planet) {
-						 maxx = maxx < planet.x ? planet.x : maxx;
-						 maxy = maxy < planet.y ? planet.y : maxy;
-						 minx = minx > planet.x ? planet.x : minx;
-						 miny = miny > planet.y ? planet.y : miny;
-					 }
-					 var planets = this.map.planets;
-					 $A(planets).each(m);
-					 var w = maxx - minx;
-					 var h = maxy - miny;
-					 var mx = this.clientWidth / w;
-					 var my = this.clientHeight / h;
-					 var dx = this.planetMaxRadius;
-					 var dy = this.planetMaxRadius;
-					 var me = this;
-					 var spares = {};
-					 var planet_ind = 0;
-					 var f = function(planet) {
-						 spares[planet_ind] = {};
-						 var e = function(nb) {
-							 if(spares[nb] == null || spares[nb][planet_ind] == null) {
-								 //canvas.line(planet.x*mx + dx, planet.y*my + dy, planets[nb].x*mx + dx, planet[nb].y*my + dy);
-								 var t = new Template('M#{x0} #{y0}L#{x1} #{y1}');
-								 try {
-								 canvas.path(t.evaluate({
-									x0: planet.x*mx + dx,
-								   	y0: planet.y*my + dy,
-								   	x1: planets[nb].x*mx + dx,
-								   	y1: planets[nb].y*my + dy
-								 }));
-								 spares[planet_ind][nb] = 1; } catch(err) { alert(err) }
-							 }
-						 };
-						 $A(planet.neighbors).each(e);
-						 var c = canvas.circle(planet.x*mx + dx , planet.y*my + dy , planet.size*me.planetRadius);
-						 c.attr('fill', '#A9A9A9');
-						 planet_ind = planet_ind + 1;
-					 };
-					 $A(planets).each(f);
-				 },
 		// data retrieve
 		loginInfo: function() {
 					   return { host: $('host').value, user: $('user').value }; 
@@ -145,7 +225,7 @@ var WSUI = new Class.create({
 						 return { 
 							gameName: $('gamename').value,
 							mapName: $('mapname').value,
-							maxPlayers: $('maxplayers').value,
+							maxPlayers: $('maxplayers').value
 						 }
 					 }
 	});
